bef7d4f2640ab21f345c7f964877dd41179e401d
KAS-L1 <kasl.54370906@gmail.com>
2024-09-07 10:51:21 +0800

    done in inheritance

--- 05-oop/02-access-modifiers-getters-setters/index.php
+++ 05-oop/02-access-modifiers-getters-setters/index.php
@@ -1,47 +1,57 @@
 <?php
 
+// Let's create a new class called "User"
 class User
 {
-  // Properties
-  public $name;
-  public $email;
-  private $status = 'active';
+  // These are like special boxes where we can store information about the user
+  public $name; // This box is for the user's name
+  public $email; // This box is for the user's email
+  private $status = 'active'; // This box is for the user's status (like "active" or "inactive")
 
+  // This is a special function that runs when we create a new user
   public function __construct($name, $email)
   {
+    // We're putting the user's name and email into their special boxes
     $this->name = $name;
     $this->email = $email;
   }
 
-  // Methods
+  // This is a function that makes the user log in
   public function login()
   {
+    // We're printing out a message that says the user logged in
     echo $this->name . ' logged in <br>';
   }
 
-  // Getter
+  // This is a function that gets the user's status
   public function getStatus(){
+    // We're printing out the user's status
     echo $this->status;
   }
 
-  // Setter
+  // This is a function that sets the user's status
   public function setStatus($status){
-  $this->status = $status;
+    // We're putting the new status into the user's special box
+    $this->status = $status;
   }
 
 }
 
-// Instantiate a new object
+// We're creating a new user named John Doe with the email john@gmail.com
 $user1 = new User('John Doe', 'john@gmail.com');
 
+// We're making John Doe log in
 $user1->login();
 
+// We're creating a new user named Jane Doe with the email jane@gmail.com
 $user2 = new User('Jane Doe', 'jane@gmail.com');
 
+// We're making Jane Doe log in
 $user2->login();
 
+// We're setting Jane Doe's status to "inactive"
 $user2->setStatus('inactive');
+
+// We're getting Jane Doe's status
 $user2->getStatus();
 
-
-// var_dump($user2);
--- 05-oop/03-inheritance/index.php
+++ 05-oop/03-inheritance/index.php
@@ -17,3 +17,31 @@ class User
     echo $this->name . ' logged in <br>';
   }
 }
+
+
+class Admin extends User {
+  public $level;
+
+  public function __construct($name, $email, $level) {
+    $this->level = $level;
+    parent::__construct($name, $email);
+  }
+
+  public function login()
+  {
+    echo 'Admin'. $this->name. 'logged in <br>';
+  }
+  public function getStatus()
+  {
+    echo $this->status;
+  }
+}
+
+$admin1 = new Admin('Tom Smith', 'Tom@gmail.com', 5);
+
+echo $admin1->name;
+echo $admin1->email;
+echo $admin1->level;
+$admin1->login();  
+$admin1->getStatus();
+
